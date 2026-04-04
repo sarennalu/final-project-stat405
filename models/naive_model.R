235860d6056762f4616faf055059119040801c71
sulman01 <sulman01@student.ubc.ca>
2026-04-04 15:50:53 -0700

    enforcing feature constraints before calling stan model

--- models/naive_model.R
+++ models/naive_model.R
@@ -1,2 +1,16 @@
+# imports
+library(dplyr)
+
 data_processed = read.csv("../data/processed/oasis_processed.csv")
-head(data_processed)
+
+# selecting relevant columns and enforcing feature constraints
+data <- data_processed %>%
+  select(sex_male, Age, nWBV, Educ, CDR_binary) %>%
+  filter(Educ > 1,
+         Educ < 5,
+         Age > 0,
+         nWBV > 0,
+         (sex_male == 1 | sex_male == 0),
+         (CDR_binary == 1 | CDR_binary == 0)
+         )
+ 
